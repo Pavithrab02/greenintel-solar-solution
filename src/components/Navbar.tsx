b5f7878636bf07f5745f90b183efee1c49960762
Pavithra B <pavb@BLR.ALLEGISINDIA.COM>
2025-04-23 14:59:18 +0530

    changes made

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,7 +31,7 @@ const Navbar = () => {
         {/* Logo */}
         <Link to="/" className="flex items-center space-x-2">
           <img 
-            src="public\greenintel.png"  // Ensure this path is correct and points to the transparent logo
+            src="dist\assets\greenintel.png"  // Ensure this path is correct and points to the transparent logo
             alt="GreenIntel Logo"
             className=" max-h-10 w-auto bg-transparent" // Adjust size as needed
           />
